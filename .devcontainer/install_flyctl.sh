7fb01ed1116904fe264219af48a527a5690bda13
thelukewhiting <hello@lukewhiting.com>
2024-07-13 16:41:53 -0700

    source

--- .devcontainer/install_flyctl.sh
+++ .devcontainer/install_flyctl.sh
@@ -3,3 +3,9 @@ set -e
 
 # Install Flyctl
 curl -L https://fly.io/install.sh | sh
+
+# Add Flyctl to PATH
+echo 'export FLYCTL_INSTALL="/home/vscode/.fly"' >> /home/vscode/.bashrc
+echo 'export PATH="$FLYCTL_INSTALL/bin:$PATH"' >> /home/vscode/.bashrc
+
+source ~/.bashrc"
