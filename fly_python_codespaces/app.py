7b7e773450e07edacac0b2a52464eb2c1c6d33d2
thelukewhiting <hello@lukewhiting.com>
2024-07-13 15:47:30 -0700

    lulz

--- fly_python_codespaces/app.py
+++ fly_python_codespaces/app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def hello():
     return "Hello, Fly.io and GitHub Codespaces!"
 
-if __name__ == "__main__":
+def main():
     app.run(host='0.0.0.0', port=8080)
+
+if __name__ == "__main__":
+    main()
