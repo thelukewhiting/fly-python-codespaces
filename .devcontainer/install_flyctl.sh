ae4ab5093cac226d0027cf55c7640c2985a8d92a
thelukewhiting <hello@lukewhiting.com>
2024-07-13 16:02:31 -0700

    :p

---
+++ .devcontainer/install_flyctl.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -L https://fly.io/install.sh | sh
--- fly_python_codespaces/app.py
+++ fly_python_codespaces/app.py
@@ -1,13 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello, Fly.io and GitHub Codespaces!"
-
 def main():
-    app.run(host='0.0.0.0', port=8080)
+    print("Hello, Fly.io and GitHub Codespaces!")
 
 if __name__ == "__main__":
     main()
