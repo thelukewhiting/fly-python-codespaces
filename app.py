1e2581dd0ffaa519da66cc18292e0700806c8680
thelukewhiting <hello@lukewhiting.com>
2024-07-13 15:43:56 -0700

    lol

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello, Fly.io and GitHub Codespaces!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Fly.io and GitHub Codespaces!"
 
 if __name__ == "__main__":
-    main()
+    app.run(host='0.0.0.0', port=8080)
